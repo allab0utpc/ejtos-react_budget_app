9e0649b8be4d2e4bce10475f6b44a3cb433a82ec
Doh <140872301+allab0utpc@users.noreply.github.com>
2024-02-04 21:28:29 +0800

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,31 +1,53 @@
 import React, { useContext } from 'react';
+import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
-import { FaTimesCircle } from 'react-icons/fa';
+import "./Buttons.css"
 
 const ExpenseItem = (props) => {
-    const { dispatch, Location} = useContext(AppContext);
+    const { dispatch, currency } = useContext(AppContext);
 
-    const handleDeleteItem = () => {
-        const item = {
-            name: props.name,
-        };
-
+    const handleDeleteExpense = () => {
         dispatch({
-            type: 'DELETE_ITEM',
-            payload: item,
+            type: 'DELETE_EXPENSE',
+            payload: props.id,
         });
     };
 
+    const increaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        });
+
+    }
+
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+
+    }
 
     return (
-        <tr>
-        <td>{props.name}</td>
-        <td>{props.quantity}</td>
-        <td>{Location}{parseInt(props.unitprice)}</td>
-        <td>{Location}{parseInt(props.quantity)*parseInt(props.unitprice)}</td>
-        <td><FaTimesCircle size='2.2em' color="red" onClick={handleDeleteItem}></FaTimesCircle></td>
-        </tr>
-    );
+		<tr>
+		<td>{props.name}</td>
+		<td>{currency}{props.cost}</td>
+		<td align="center"><button className = "btn-plus" onClick={event=> increaseAllocation(props.name)}>+</button></td>
+		<td align="center"><button className = "btn-minus" onClick={event=> decreaseAllocation(props.name)}>-</button></td>
+		<td align="center"><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+		</tr>
+	);
 };
 
 export default ExpenseItem;
